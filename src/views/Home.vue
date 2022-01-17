4ed6f92f8945e7ed210e09bb0c1830ee2318a098
= <=>
2022-01-17 22:06:51 +0100

    wip home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <v-container> Home </v-container>
+  <v-container id="index" class="py-0">
+    <div class="index d-flex flex-column justify-center align-center">
+      <v-avatar :size="`${onMobile ? 256 : 384}`">
+        <v-img :src="require('../assets/me.jpeg')" />
+      </v-avatar>
+      <div class="typewriter">
+        <p>Hello World!</p>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "Home",
-};
+  name: 'Home',
+}
 </script>
